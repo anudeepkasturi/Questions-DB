26d0d2bf3fd05bf6c0219a567ec8f0b7a6f70d14
App Academy Student <student@appacademy.io>
2016-09-27 11:25:35 -0700

    finish medium difficulty queries

--- questions.rb
+++ questions.rb
@@ -55,4 +55,36 @@ class Question
     Reply.find_by_question_id(@id)
   end
 
+  def followers
+    QuestionFollow.followers_for_question_id(@id)
+  end
+
+end
+
+class QuestionFollow
+  def self.followers_for_question_id(question_id)
+    results = QuestionsDB.instance.execute(<<-SQL, question_id)
+      SELECT
+        user_id
+      FROM
+        questions_follows
+      WHERE
+        question_id = ?
+    SQL
+
+    results.map { |result| User.find_by_id(result) }
+  end
+
+  def self.followed_questions_for_user_id(user_id)
+    results = QuestionsDB.instance.execute(<<-SQL, user_id)
+      SELECT
+        question_id
+      FROM
+        questions_follows
+      WHERE
+        user_id = ?
+    SQL
+
+    results.map { |result| Question.find_by_id(result) }
+  end
 end
